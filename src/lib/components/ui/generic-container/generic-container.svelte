487a4a5372163fbdc48a2cd9eb18b033a6b51f52
Matthew Franchini <matchewf11@gmail.com>
2025-10-27 16:39:37 -0700

    will this work?

--- src/lib/components/ui/generic-container/generic-container.svelte
+++
@@ -1,4 +0,0 @@
-<script lang="ts">
-</script>
-
-<div class="rounded-xl text-secondary m-3 p-3 bg-primary/50"></div>
--- src/routes/overview/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-</script>
-
-<div>
-	<h1>Probably not needed</h1>
-</div>
